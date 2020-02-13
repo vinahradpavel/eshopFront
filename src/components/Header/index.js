96d26c8e21ba789481d852d739aca3a305697d14
Pavel Vinahrad <pavel.vinahrad@instinctools.com>
2020-02-13 17:30:13 +0300

    up

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import styles from './style.module.css';
 import Navigation from '../Navigation';
+import Menu from '../Menu';
+
 
 
 const Header = (props) => (
 <div className={styles.Header}>
-    <Navigation/>
+    <Navigation>  
+        <Menu/>
+    </Navigation>
 </div>
 )
 
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Menu from './../Menu'
 import styles from './style.module.css';
 
 
 const Navigation = (props) => (
 <div className={styles.Container}>
     <h1>{props.text}</h1>
-    <Menu/>
 </div>
 )
 
